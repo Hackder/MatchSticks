4afbd9524b9324ed8e62ac349c97c33d419b5e53
Hackder <dods1472@protonmail.com>
2020-11-19 23:12:42 +0100

    added reset button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { StickComponent } from './components/stick/stick.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, StickComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, DragDropModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
